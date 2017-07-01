8acc6e0c688924d2d0afe41fb9736df9c3fba4b8
Cythral <talen@cythral.com>
2017-07-01 21:54:58 +0000

    fix ajax glitch

--- src/constants.php
+++ src/constants.php
@@ -2,7 +2,7 @@
 namespace Phroses;
 
 define("Phroses", true);
-define("Phroses\VERSION", "v0.4.8");
+define("Phroses\VERSION", "v0.4.9");
 define("Phroses\SRC", __DIR__);
 define("Phroses\SCHEMAVER", 2);
 define("Phroses\DEPS", $deps);
