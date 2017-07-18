4a472571761bcebd25d530bcae072cbf8ac3e81d
Cythral <talen@cythral.com>
2017-07-18 15:17:14 +0000

    update ver

--- src/constants.php
+++ src/constants.php
@@ -2,7 +2,7 @@
 namespace Phroses;
 
 define("Phroses", true);
-define("Phroses\VERSION", "v0.4.20");
+define("Phroses\VERSION", "v0.5.0");
 define("Phroses\SRC", __DIR__);
 define("Phroses\SCHEMAVER", 2);
 define("Phroses\DEPS", $deps);
