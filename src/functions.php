ccb40b3ea05cad6a8cc7e0bc498c80ce1b4b429a
Cythral <talen@cythral.com>
2017-11-18 17:56:21 +0000

    remove expires header

--- src/functions.php
+++ src/functions.php
@@ -92,6 +92,7 @@ function ReadfileCached($file) {
     header("Last-Modified: $gmt_mtime");
     header("Etag: $etag");
     header_remove("pragma");
+    header_remove("expires");
     
     if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) || isset($_SERVER['HTTP_IF_NONE_MATCH'])) {
         if ($_SERVER['HTTP_IF_MODIFIED_SINCE'] == $gmt_mtime || str_replace('"', '', stripslashes($_SERVER['HTTP_IF_NONE_MATCH'])) == $etag) {
