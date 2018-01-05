e2a75cdb2cb39e4d5c51a4a7535293af16681c05
Cythral <talen@cythral.com>
2018-01-05 17:52:52 +0000

    newsite cleanup

--- src/system/newsite.php
+++ src/system/newsite.php
@@ -1,67 +1,27 @@
 <?php
 
-Phroses\HandleMethod("POST", function() {
-  $out = new \reqc\JSON\Server();
+use \Phroses\DB;
+use \phyrex\Template;
+use function \Phroses\handleMethod;
+use const \Phroses\{ SRC, INCLUDES };
+use const \reqc\{ BASEURL };
+
+handleMethod("post", function($out) {
   
-  Phroses\DB::Query("INSERT INTO `sites` (`url`, `theme`, `name`,`adminUsername`, `adminPassword`) VALUES (?, 'bloom', ?, ?, ?)", [
-    strtok($_SERVER["HTTP_HOST"], ":"),
+  DB::Query("INSERT INTO `sites` (`url`, `theme`, `name`,`adminUsername`, `adminPassword`) VALUES (?, 'bloom', ?, ?, ?)", [
+    BASEURL,
     $_POST["name"],
     $_POST["username"],
     password_hash($_POST["password"], PASSWORD_DEFAULT)
   ]);
   
   $out->send(["type" => "success"], 200);
+
 }, ["name", "username", "password"]);
 
-http_response_code(404);
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Create Phroses Site</title>
-    <style><?= file_get_contents(Phroses\SRC."/views/phr-assets/css/main.css"); ?></style>
-  </head>
-  <body class="aln-c">
-    <div class="screen" id="install-welcome">
-      <h1>Welcome</h1>
-      <div></div>
-    </div>
-    <div id="install-flow">
-      <h1 class="c">Create a Site</h1>
-      <p>I couldn't find a site at <?= reqc\HOST; ?>, so I'll help you create one.</p>
-      
-      <form action="" method="post" id="flow-site">
-        <div class="form_icfix c aln-l">
-          <div>Site Name:</div>
-          <input class="form_field form_input" placeholder="Phroses" name="name" id="name" required>
-          <div class="clear"></div>
-        </div>
-        
-        <div class="form_icfix c aln-l">
-          <div>Username:</div>
-          <input class="form_field form_input" placeholder="Username" name="username" id="susername" required autocomplete="new-password">
-          <div class="clear"></div>
-        </div>
-        
-        <div class="form_icfix c aln-l">
-          <div>Password:</div>
-          <input class="form_field form_input" placeholder="Password" name="password" id="spassword" required type="password" autocomplete="new-password">
-          <div class="clear"></div>
-        </div>
-        <br>
-        <button class="pst_btn txt">
-        Submit
-        </button>
-      </form>
-    </div>
-    
-    <div id="flow-success" class="screen">
-      <h2 class="c">Success</h2>
-    </div>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script><?= file_get_contents(Phroses\SRC."/views/phr-assets/js/install.js"); ?></script>
-  </body>
-</html>
-<?php die;
+self::$out->setCode(404);
+$newsite = new Template(INCLUDES["TPL"]."/newsite.tpl");
+$newsite->url = BASEURL;
+$newsite->styles = file_get_contents(SRC."/views/phr-assets/css/main.css");
+$newsite->script = file_get_contents(SRC."/views/phr-assets/js/install.js");
+die($newsite);
---
+++ src/templates/newsite.tpl
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Create Phroses Site</title>
+    <style><{var::styles}></style>
+  </head>
+  <body class="aln-c">
+    <div class="screen" id="install-welcome">
+      <h1>Welcome</h1>
+      <div></div>
+    </div>
+    <div id="install-flow">
+      <h1 class="c">Create a Site</h1>
+      <p>I couldn't find a site at <{var::url}>, so I'll help you create one.</p>
+      
+      <form action="" method="post" id="flow-site">
+        <div class="form_icfix c aln-l">
+          <div>Site Name:</div>
+          <input class="form_field form_input" placeholder="Phroses" name="name" id="name" required>
+          <div class="clear"></div>
+        </div>
+        
+        <div class="form_icfix c aln-l">
+          <div>Username:</div>
+          <input class="form_field form_input" placeholder="Username" name="username" id="susername" required autocomplete="new-password">
+          <div class="clear"></div>
+        </div>
+        
+        <div class="form_icfix c aln-l">
+          <div>Password:</div>
+          <input class="form_field form_input" placeholder="Password" name="password" id="spassword" required type="password" autocomplete="new-password">
+          <div class="clear"></div>
+        </div>
+        <br>
+        <button class="pst_btn txt">
+        Submit
+        </button>
+      </form>
+    </div>
+    
+    <div id="flow-success" class="screen">
+      <h2 class="c">Success</h2>
+    </div>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script><{var::script}></script>
+  </body>
+</html>
