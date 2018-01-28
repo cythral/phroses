fb42cae92854cb95f3bc3561656304161248b19f
Cythral <talen@cythral.com>
2018-01-28 22:14:40 +0000

    increase version

--- src/constants.php
+++ src/constants.php
@@ -4,7 +4,7 @@ namespace Phroses;
 use \reqc;
 
 define("Phroses", true);
-define("Phroses\VERSION", "v0.5.26");
+define("Phroses\VERSION", "v0.5.27");
 define("Phroses\SRC", __DIR__);
 define("Phroses\SCHEMAVER", 2);
 define("Phroses\DEPS", $deps);
