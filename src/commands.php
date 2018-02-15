643143493b81b88bf522e0b0ced6a8a25b72735f
Cythral <talen@cythral.com>
2018-02-15 22:03:04 +0000

    document constants.php

--- src/commands.php
+++ src/commands.php
@@ -1,29 +1,42 @@
 <?php
+/**
+ * This file sets up CLI commands
+ */
 
 namespace Phroses;
 
 use \ZBateson\MailMimeParser\MailMimeParser;
 
+/**
+ * Turns application-wide maintenance mode off and on
+ */
 self::addCmd("maintenance", function($args, $flags) {
 	if(isset($args["mode"])) {
 		self::setMaintenance(mapValue(strtolower($args["mode"]), [ "on" => self::MM_ON, "off" => self::MM_OFF ]));
 	}
 });
 
+/**
+ * Updates phroses' database schema
+ */
 self::addCmd("update", function($args, $flags) {
 	DB::Update();
 });
 
+/**
+ * Processes an email that was piped to phroses.  There is
+ * no default functionality for this, so a listen event is triggered instead.
+ */
 self::addCmd("email", function($args, $flags) {
 	$data = file_get_contents("php://stdin");
-	$m = (new MailMimeParser())->parse((string) $data);
+	$email = (new MailMimeParser())->parse((string) $data);
 
 	Events::trigger("email", [
-		$m->getHeaderValue('from'),
-		$m->getHeaderValue('to'),
-		$m->getHeaderValue('subject'),
-		$m->getTextContent() || $m->getHtmlContent()
+		$email->getHeaderValue('from'),
+		$email->getHeaderValue('to'),
+		$email->getHeaderValue('subject'),
+		$email->getTextContent() || $email->getHtmlContent()
 	]);
 });
 
-return self::$cmds;
+return self::$cmds; // return a list of commands for the listen event
