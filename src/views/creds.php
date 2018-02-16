2cfaf000d9bee633e2b0e72f6a61badb9b907660
Cythral <talen@cythral.com>
2018-02-16 17:15:30 +0000

    fix tabbing, use mapError views/creds.php

--- src/views/creds.php
+++ src/views/creds.php
@@ -7,28 +7,28 @@ use function Phroses\{ handleMethod };
 use const Phroses\{ SITE, INCLUDES };
 
 handleMethod("post", function($out) {
-  if($_POST["username"] == "") $out->send(["type" => "error", "error" => "bad_value", "field" => "username" ], 400);
-	
-  if($_POST["old"] != "" || $_POST["new"] != "" || $_POST["repeat"] != "") {
-    if(!password_verify(inix::get("pepper").$_POST["old"], SITE["PASSWORD"])) $out->send(["type" => "error", "error" => "bad_value", "field" => "old"], 400);
-    if(strlen($_POST["new"]) > 50) $out->send(["type" => "error", "error" => "pw_length"], 400);
-    if($_POST["new"] != $_POST["repeat"]) $out->send(["type" => "error", "error" => "bad_value", "field" => "repeat"], 400);
-    
-    DB::Query("UPDATE `sites` SET `adminPassword`=? WHERE `id`=?", [
-      password_hash(inix::get("pepper").$_POST["new"], PASSWORD_DEFAULT),
-      SITE["ID"]
+    mapError("bad_value", empty($_POST["username"]), [ "field" => "username" ]);
+
+    if($_POST["old"] != "" || $_POST["new"] != "" || $_POST["repeat"] != "") {
+        mapError("bad_value", !password_verify(inix::get("pepper").$_POST["old"], SITE["PASSWORD"]), [ "field" => "old" ]);
+        mapError("pw_length", strlen($_POST["new"]) > 50);
+        mapError("bad_value", $_POST["new"] != $_POST["repeat"], [ "field" => "repeat" ]);
+
+        DB::Query("UPDATE `sites` SET `adminPassword`=? WHERE `id`=?", [
+            password_hash(inix::get("pepper").$_POST["new"], PASSWORD_DEFAULT),
+            SITE["ID"]
+        ]);
+    }
+
+    DB::Query("UPDATE `sites` SET `adminUsername`=? WHERE `id`=?", [
+        $_POST["username"],
+        SITE["ID"]
     ]);
-  }
-  
-  DB::Query("UPDATE `sites` SET `adminUsername`=? WHERE `id`=?", [
-    $_POST["username"],
-    SITE["ID"]
-  ]);
-  
-  $out->send(["type" => "success"], 200);
-  
+
+    $out->send(["type" => "success"], 200);
+
 }, ["username", "old", "new", "repeat"]);
- 
+
 
 $creds = new Template(INCLUDES["TPL"]."/admin/creds.tpl");
 $creds->username = SITE["USERNAME"];
