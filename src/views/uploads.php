3af3d8521a0ca97097f0d7b2858b7922d52ff8b0
Cythral <talen@cythral.com>
2018-02-16 17:00:32 +0000

    use mapError in uploads

--- src/views/uploads.php
+++ src/views/uploads.php
@@ -1,32 +1,28 @@
 <?php
 
 use phyrex\Template;
-use function Phroses\{ handleMethod };
+use function Phroses\{ handleMethod, parseSize, mapError };
 use const Phroses\{ INCLUDES, SITE };
 
 $uploaddir = INCLUDES["UPLOADS"]."/".reqc\BASEURL."/";
 
 handleMethod("post", function($out) use ($uploaddir) {
-
-    $error = function (string $error, bool $check, array $extra = []) use ($out) {
-        if($check) $out->send(array_merge(["type" => "error", "error" => $error], $extra), 400);
-    };
-
+    
     if($_POST['action'] == "rename") {
-        $error("resource_exists", file_exists($uploaddir.$_POST["to"]));
-        $error("failed_rename", !@rename($uploaddir.$_POST["filename"], $uploaddir.$_POST["to"]));
+        mapError("resource_exists", file_exists($uploaddir.$_POST["to"]));
+        mapError("failed_rename", !@rename($uploaddir.$_POST["filename"], $uploaddir.$_POST["to"]));
     }
 
     if($_POST["action"] == "delete") {
-       $error("failed_delete", !@unlink($uploaddir.$_POST["filename"]));
+       mapError("failed_delete", !@unlink($uploaddir.$_POST["filename"]));
     }
 
     if($_POST["action"] == "new") {
-        $error("topupldir_notfound", !file_exists(INCLUDES["UPLOADS"]) && !@mkdir(INCLUDES["UPLOADS"]));
-        $error("siteupldir_notfound", !file_exists($uploaddir) && !@mkdir($uploaddir));
-        $error("resource_exists", file_exists($uploaddir.$_POST["filename"]));
-        $error("large_file", in_array($_FILES["file"]["error"], [ UPLOAD_ERR_INI_SIZE, UPLOAD_ERR_FORM_SIZE ]));
-        $error("failed_upl", !move_uploaded_file($_FILES["file"]["tmp_name"], $uploaddir.$_POST["filename"]));
+        mapError("topupldir_notfound", !file_exists(INCLUDES["UPLOADS"]) && !@mkdir(INCLUDES["UPLOADS"]));
+        mapError("siteupldir_notfound", !file_exists($uploaddir) && !@mkdir($uploaddir));
+        mapError("resource_exists", file_exists($uploaddir.$_POST["filename"]));
+        mapError("large_file", in_array($_FILES["file"]["error"], [ UPLOAD_ERR_INI_SIZE, UPLOAD_ERR_FORM_SIZE ]));
+        mapError("failed_upl", !move_uploaded_file($_FILES["file"]["tmp_name"], $uploaddir.$_POST["filename"]));
     }
 
     $out->send(["type" => "success"], 200);
