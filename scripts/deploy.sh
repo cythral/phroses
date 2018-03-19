0ba35714d0c88e5b706cd15b2d8c382e3d881d5e
Cythral <talen@cythral.com>
2018-03-19 17:22:34 -0500

    deploy to deb.cythral.com

---
+++ scripts/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+
+eval "$(ssh-agent -s)"
+chmod 600 config/deploy.key
+ssh-add config/deploy.key
+
+ssh travis@deb.cythral.com <<PHRS
+    cd phroses 
+    composer install
+    export PATH=\$PATH:\$PWD/vendor/bin:\$(npm bin)
+    git pull origin $TRAVIS_BRANCH; 
+    composer run build -- $TRAVIS_BRANCH
+    
+    aptly repo add stretch phroses-$TRAVIS_BRANCH.deb
+    aptly -batch=true publish update stretch
+    git clean -xdf
+PHRS
+
+else
+    
+echo "Skipping deployment since this is not a tagged commit"
+
+fi
